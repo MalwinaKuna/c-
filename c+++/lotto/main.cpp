698938620e69de72ff8184ffcba05f52d27ff27a
Your Name <you@example.com>
2019-02-01 14:41:09 +0100

    q

--- c+++/lotto/main.cpp
+++ c+++/lotto/main.cpp
@@ -9,16 +9,18 @@ int main()
 {
    //#include <thread> //nie potrzeba//
 //#include <chrono> //nie potrzeba//
+cout<<"Za chwile zwolni sie blokada losowania"<<endl;
+usleep(1000000);
 
-{
 srand(time(NULL));
-for(int i=1; i<=6; i++){
-liczba = rand()%49+1;
+for(int i=1; i<=6; i++)
+    {
+        liczba = rand()%49+1;
         usleep(1000000); //na windowie to będzie Sleep(1000);//
-        cout<<liczba<<"\a"<<endl;
+        cout<<liczba<<endl;
         //fflush(stdout);//
     }
 getchar();getchar();
     return 0;
-}
+
 }
