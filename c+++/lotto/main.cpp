1e0a3a3622d035f3833ed1de7effaeb236e9daa0
Your Name <you@example.com>
2019-02-02 09:26:44 +0100

    lotto

--- c+++/lotto/main.cpp
+++ c+++/lotto/main.cpp
@@ -2,13 +2,14 @@
 #include <cstdlib>
 #include <stdio.h>
 #include <unistd.h>
+  //#include <thread> //nie potrzeba//
+//#include <chrono> //nie potrzeba//
 
 using namespace std;
-int liczba;
 int main()
 {
-   //#include <thread> //nie potrzeba//
-//#include <chrono> //nie potrzeba//
+int liczba;
+
 cout<<"Za chwile zwolni sie blokada losowania"<<endl;
 usleep(1000000);
 
@@ -17,10 +18,10 @@ for(int i=1; i<=6; i++)
     {
         liczba = rand()%49+1;
         usleep(1000000); //na windowie to będzie Sleep(1000);//
-        cout<<liczba<<endl;
-        //fflush(stdout);//
+        cout<<liczba<<"   ";
+        fflush(stdout);
     }
-getchar();getchar();
+getchar();
     return 0;
 
 }
